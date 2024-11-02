c7d89f5f46fdabe6aa39dfc4889a472015eec202
ilarion123 <ilaraujera@gmail.com>
2024-11-02 14:56:17 +0400

    Initial commit

---
+++ ilarion.py
@@ -0,0 +1,138 @@
+#1) question="what do you call a bear with no teeth?"
+# answer="A gummy bear!"
+# print(f"question:{question} answer:{answer}")
+
+#2) x=eval(input("enter a number1:"))
+# y=eval(input("enter a number2:"))
+# jami=x+y
+# print("total is:",jami)
+
+#3) x=eval(input("enter number1:"))
+# y=eval(input("enter number2:"))
+# z=eval(input("enter number3:"))
+# print(((x+y)*z))
+
+#4) sul_nawrebi= int(input("How many pieces of pizza do you have? "))
+# nawami_najrebi = int(input("How many pieces have you eaten? "))
+# darchenili_nawrebi=sul_nawrebi-nawami_najrebi
+# print("a piece of pizza left:",darchenili_nawrebi)
+
+#5) kilogrami=eval(input("Enter your weight in kilograms: "))
+# funti= kilogrami* 2204
+# print("Your weight in funt is:", funti)
+
+
+
+
+#7) ricxvi=int(input("sheiyvane ricxvebi atidan ocis chatvlit:"))
+# if ricxvi>=10 and ricxvi<=20:
+#    print("thank you")
+# else:
+#     print("incorrect answer")
+
+#8) name=str(input("sheiyvane sheni saxeli:"))
+# surname=str(input("sheiyvane sheni gvari:"))
+# age=int(input("sheiyvane sheni asaki:"))
+# if age>=18:
+#     print(f'{name} cavn vote')
+# if age==17:
+#     print(f'{name,surname} can learn to drive')
+# if age==16:
+#     print(f"{name} can go Trick-or Treating")
+# else:
+#     print("nothing")
+
+#6) Num1=eval(input("sheiyvane ricxvi:"))
+# if Num1>10:
+#     print("This is over 10")
+# elif Num1==10:
+#     print("This is equal to 10")
+# else:
+#     print("This is under 10") 
+
+#9) F=float(input("Sheiyvane temperatura faregeitshi:"))
+# C=(F-32)*5/9
+# wvims=input('wvims?')
+# print(C)
+# mzea=input("mze aris?")
+# if C>20 and wvims=='ki':
+#     print("Wear thinly and take umbrella")
+# elif C>20:
+#     print("Wear thinly")
+# elif -10<C<10 and wvims=='ki':
+#     print("Take umbrella and wear warmly")
+# elif -10<C<10:
+#     print("wear warmly")
+# elif C>100 or C<-10 and wvims=='ki':
+#     print("you're died")
+# elif not mzea=='ki':
+#     print("ar gchirdeba satvale")
+# elif mzea=='ki':
+#     print("gchirdeba satvale")
+# else:
+#     print("You dont need umbrella")
+
+
+#10) a=int(input("sheiyvane ricxvi 1:"))
+# b=int(input("sheiyvane ricxvi 2:"))
+# c=int(input("sheiyvane ricxvi 3:"))
+# if a%2==0:
+#     print("a luwia")
+# else:
+#     print("a kentia")
+# if b%2==0:
+#     print("b luwia")
+# else:
+#     print("b kentia")
+# if c%2==0:
+#     print("c luwia")
+# else:
+#     print("c kentia")
+# if a%7==2:
+#     print("a/7 nashti aris 2")
+# else:
+#     print("a/7 nashti ar aris 2")
+# if a%7==3:
+#     print("a/7 nashti aris 3")
+# else:
+#     print("a/7 nashti ar aris 3")
+# if a%7==4:
+#     print("a/7 nashti aris 4")
+# else:
+#     print("a/7 nashti ar aris 4")
+# if a%7==5:
+#     print("a/7 nashti aris 5")
+# else:
+#     print("a/7 nashti ar aris 5")
+# if b%7==2:
+#     print("b/7 nashti aris 2")
+# else:
+#     print("b/7 nashti ar aris 2")
+# if b%7==3:
+#     print("b/7 nashti aris 3")
+# else:
+#     print("b/7 nashti ar aris 3")
+# if b%7==4:
+#     print("b/7 nashti aris 4")
+# else:
+#     print("b/7 nashti ar aris 4")
+# if a%7==5:
+#     print("b/7 nashti aris 5")
+# else:
+#     print("b/7 nashti ar aris 5")
+# if c%7==2:
+#     print("c/7 nashti aris 2")
+# else:
+#     print("c/7 nashti ar aris 2")
+# if c%7==3:
+#     print("c/7 nashti aris 3")
+# else:
+#     print("c/7 nashti ar aris 3")
+# if c%7==4:
+#     print("c/7 nashti aris 4")
+# else:
+#     print("c/7 nashti ar aris 4")
+# if c%7==5:
+#     print("c/7 nashti aris 5")
+# else:
+#     print("c/7 nashti ar aris 5")
