a56b392a31de8b206facfcf96bea95783c7ce996
ilarion ujerashvili <ilaraujera@gmail.com>
2024-11-24 23:40:57 +0400

    Add all my files

---
+++ leqcia7-is davaleba.py
@@ -0,0 +1,73 @@
+# შეიტანეთ ათწილადი რიცხვი, დაამრგვალეთ ათწილად ნაწილში მეათედის სიზუსტით (1 ციფრი
+# ათწილად ნაწილში) და დაბეჭდეთ შედეგი. გამოიყენეთ round, ceil, floor, trunc ფუნქციები სათითაოდ
+# # და შეამოწმეთ შედეგი თითოეულის გამოყენებით. 
+
+# x=float(input("enter number:"))
+# y=round(x,1)
+# print(y)
+
+# import math
+
+# x=float(input("enter number:"))
+# y=math.ceil(x)
+# print(y)
+
+
+# import math
+
+# x=float(input("enter number:"))
+# y=math.floor(x)
+# print(y)
+
+# import math
+
+# x=float(input("enter number:"))
+# y=math.trunc(x)
+# print(y)
+
+# შეიტანეთ სამი რიცხვი, იპოვეთ მათ შორის მაქსიმუმი და დაბეჭდეთ შედეგი. გამოიყენეთ max
+# ფუნქცია. 
+
+# x=eval(input("enter a number:"))
+# y=eval(input("enter a number:"))
+# z=eval(input("enter a number:"))
+# c=max(x,y,z)
+# print(c)
+
+# გამოთვალეთ 3 ფუნქციის გამოყენებით და დაბეჭდეთ შედეგი. ასევე გამოთვალეთ 2, 4 და დაბეჭდეთ
+# მიღებული შედეგი.
+
+# გამოთვალეთ შემდეგი გამოსახულების შედეგი მათემატიკური ფუნქციის გამოყენებით: 
+# a)
+# import math
+# print(math.sqrt(225625))
+# b)
+# import math
+# print(math.sqrt(4225))
+
+# დააგენერირეთ ნებისმიერი შემთხვევითი ათწილადი რიცხვი დიაპაზონიდან 0-დან 1-ის ჩათვლით.
+# დაამრგვალეთ რიცხვი (3 ციფრი ათწილად ნაწილში) და დაბეჭდეთ. 
+
+# import random
+
+# x=random.random()
+# y=round(x, 3)
+# print(y)
+
+# დააგენერირეთ ნებისმიერი შემთხვევითი ათწილადი რიცხვი 100-დან 120-მდე. დაამრგვალეთ
+# რიცხვი (1 ციფრი ათწილად ნაწილში) და ისე გამოიტანეთ. 
+
+# import random
+
+# x=random.uniform(100,120)
+# y=round(x,1)
+# print(y)
+
+# დააგენერირეთ 10 შემთხვევითი მთელი რიცხვი და დაბეჭდეთ ეკრანზე. მითითება: გამოიყენეთ
+# ციკლის ოპერატორი.
+
+# import random
+
+# for i in range(10):
+#     x=random.randint(0, 100000)
+#     print(x,)
