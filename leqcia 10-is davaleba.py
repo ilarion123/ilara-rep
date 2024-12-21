d061574a6b0ae53b4eb5c1c98295a7f25239c987
ilarion ujerashvili <ilaraujera@gmail.com>
2024-12-21 20:40:43 +0400

    Add all my files

---
+++ leqcia 10-is davaleba.py
@@ -0,0 +1,40 @@
+#1)
+# arr={0: 10, 1: 20}
+# arr[2]=30
+# arr.update({3:40})
+# print(arr)
+# del arr[3]
+# print(arr)
+
+# 2)
+
+# dic1 = {1: 10, 2: 20}
+# dic2 = {3: 30, 4: 40}
+# dic3 = {5: 50, 6: 60}
+# dic1.update(dic2)
+# dic1.update(dic3)
+# print(dic1)
+
+#3)
+
+# d = {1: 10,2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+# if 1 in d:
+#     print("elementi arsebobs")
+# else:
+#     print("ar arsebobs")
+# if 7 not in d:
+#     print("ar arsebobs")
+# else:
+#     print("arsebobs")
+
+#4)
+
+# d = {'x': 10, 'y': 20, 'z': 30}
+# for key, value in d.items():
+#     print(f"{key}->value}")
+
+#5)
+
+# d = {i: i**3 for i in range(1, 11)}
+# print(d)
+
